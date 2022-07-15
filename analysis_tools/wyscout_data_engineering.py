25c22e2e8f1c34f9b7b9dd9d55ee585ff337d9ed
jakeyk11 <jkolliari@hotmail.com>
2022-07-15 22:00:46 +0100

    Addition of first draft of project 3 - xg models

---
+++ analysis_tools/wyscout_data_engineering.py
@@ -0,0 +1,100 @@
+"""Module containing functions to assist with pre-processing and engineering of Wyscout-style data
+
+Functions
+---------
+format_wyscout_data(tournament='Italy', folder=)
+    Load Wyscout json files with matches, events, players and competitions."""
+
+import bz2
+import pickle
+from collections import defaultdict
+import pandas as pd
+
+
+def format_wyscout_data(tournament='England', data_folder="../../data_directory/wyscout_data"):
+    """ Load Wyscout json files with matches, events, players and competitions.
+
+    Function to load and format Wyscout json files (location defined by folder input) for a user-defined tournament.
+
+    Args:
+        tournament (list, optional): List of tournaments to load.
+        data_folder (str, optional): Location of data, relative to script in which function is called..
+
+    Returns:
+        pandas.DataFrame: wyscout-style event dataframe, containing all events from selected competition.
+        pandas.DataFrame: wyscout-style matches dataframe, containing all match information from selected competition.
+        defaultdict: wyscout-style event dictionary, containing events per match.
+        pandas.DataFrame: wyscout-style player dataframe, containing player info for all players in av. Wyscout data.
+        pandas.DataFrame: wyscout-style competition dataframe, containing comp info for all comps in av. Wyscout data.
+        pandas.DataFrame: wyscout-style team dataframe, containing team info for all teams in av. Wyscout data.
+    """
+
+    if isinstance(tournament, str):
+        tournament = [tournament]
+
+    events, matches = pd.DataFrame(), pd.DataFrame()
+
+    for idx, data_selection in enumerate(tournament):
+        
+        # Load in the Wyscout matches and event data
+        events_temp = bz2.BZ2File(f"{data_folder}/events/events_{data_selection}.pbz2", 'rb')
+        events_temp = pd.DataFrame(pickle.load(events_temp))
+        matches_temp = bz2.BZ2File(f"{data_folder}/matches/matches_{data_selection}.pbz2", 'rb')
+        matches_temp = pd.DataFrame(pickle.load(matches_temp))
+
+        if idx == 0:
+            events = events_temp
+            matches = matches_temp
+        else:
+            events = events.append(events_temp)
+            matches = matches.append(matches_temp)
+
+    # Produce a dictionary of lists: top level dictionary of matches with sub-list of events
+    match_id2events = defaultdict(list)
+    for _, event in events.iterrows():
+        match_id = event['matchId']
+        match_id2events[match_id].append(event)
+
+    # Produce a dictionary of dictionaries: top level dictionary of matches with sub-dictionary of match info
+    match_id2match = defaultdict(dict)
+    for _, match in matches.iterrows():
+        match_id = match['wyId']
+        match_id2match[match_id] = match
+
+    # Load in the Wyscout player data
+    players = bz2.BZ2File(f"{data_folder}/players.pbz2", 'rb')
+    players = pickle.load(players)
+
+    # Produce a dictionary of dictionaries: top level dictionary of players with sub-dictionary of player info
+    player_id2player = defaultdict(dict)
+    for player in players:
+        player_id = player['wyId']
+        player_id2player[player_id] = player
+
+    # Load in the Wyscout competition data
+    competitions = bz2.BZ2File(f"{data_folder}/competitions.pbz2", 'rb')
+    competitions = pickle.load(competitions)
+
+    # Produce a dictionary: top level dictionary of competitions with sub-dictionary of competition info
+    competition_id2competition = defaultdict(dict)
+    for competition in competitions:
+        competition_id = competition['wyId']
+        competition_id2competition[competition_id] = competition
+
+    # Load in the Wyscout teams data
+    teams = bz2.BZ2File(f"{data_folder}/teams.pbz2", 'rb')
+    teams = pickle.load(teams)
+
+    # Produce a list of dictionaries: top level dictionary of teams with sub-dictionary of team info
+    team_id2team = defaultdict(dict)
+    for team in teams:
+        team_id = team['wyId']
+        team_id2team[team_id] = team
+
+    # Convert to dataframes
+    match_id2match = pd.DataFrame(match_id2match).transpose()
+    player_id2player = pd.DataFrame(player_id2player).transpose()
+    competition_id2competition = pd.DataFrame(competition_id2competition).transpose()
+    team_id2team = pd.DataFrame(team_id2team).transpose()
+
+    return match_id2match, events, match_id2events, player_id2player, competition_id2competition, team_id2team
---
+++ projects/03_xg_model/xg_neural_network.py
@@ -0,0 +1,185 @@
+# %% Expected Goals Model, using Wyscout data from Top 5 Leagues in 2017/18
+#
+# Inputs:   Leagues to use to train xg model
+#
+# Outputs:  xG model, displayed as an xG heatmap
+#           Dataframe all shots in chosen leagues, including shot information and xG.
+#
+# Notes: None
+
+# %% Imports
+
+import os
+import sys
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from mplsoccer.pitch import VerticalPitch
+import pickle
+import bz2
+
+# %% Add custom tools to path
+
+root_folder = os.path.abspath(os.path.dirname((os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+sys.path.append(root_folder)
+
+import analysis_tools.wyscout_data_engineering as wde
+
+# %% User inputs
+
+# List of leagues to use for xg model
+leagues = ["England", "Italy", "France", "Germany", "Spain"]
+
+# %% Set constants
+
+PITCH_WIDTH_Y = 80
+PITCH_LENGTH_X = 120
+GOAL_WIDTH_Y = 8
+
+# %% Load Wyscout data for all competitions
+
+match_data, event_data, event_data_by_match, player_data, competition_data, team_data = wde.format_wyscout_data(leagues)
+
+# %% Define dataframe of all shots in event data
+
+# Initialise shot model dataframe
+shots_model_df = pd.DataFrame()
+i = 0
+
+# Loop through shots and obtain/calculate shot information
+for _, shot in event_data[event_data['eventName']=='Shot'].iterrows():
+    
+    # Player, team and competition
+    if shot['playerId'] != 0:
+        shots_model_df.loc[i, 'player_name'] = player_data.loc[shot['playerId']]['shortName'].encode('ascii', 'strict').decode('unicode-escape')
+    else:
+        shots_model_df.loc[i, 'player_name'] = np.nan
+    shots_model_df.loc[i, 'team_name'] = team_data.loc[shot['teamId']]['name'].encode('ascii', 'strict').decode('unicode-escape')
+    shots_model_df.loc[i, 'competition_name'] = competition_data.loc[match_data.loc[shot['matchId']]['competitionId']]['name']
+    
+    # Position and distance info
+    shots_model_df.loc[i, 'x_yards'] = (PITCH_LENGTH_X/100)*(100 - shot['positions'][0]['x'])
+    shots_model_df.loc[i, 'c_yards'] = (PITCH_WIDTH_Y/100)*(shot['positions'][0]['y'] - 50)
+    shots_model_df.loc[i,'distance_yards'] = np.sqrt(shots_model_df.loc[i, 'x_yards']**2 + shots_model_df.loc[i, 'c_yards']**2)
+    
+    # Angle info
+    angle_denominator = (shots_model_df.loc[i, 'x_yards']**2 + shots_model_df.loc[i, 'c_yards']**2 - (GOAL_WIDTH_Y/2)**2)
+    if angle_denominator == 0:
+        angle = np.pi/2
+    else:
+        angle = np.arctan((2*(GOAL_WIDTH_Y/2)*shots_model_df.loc[i, 'x_yards'])/angle_denominator)
+    if angle<0:
+        angle = np.pi + angle
+    shots_model_df.loc[i, 'angle'] = angle
+    
+    # Header info
+    shots_model_df.loc[i, 'header_tag'] = 0
+    if {'id': 403} in shot['tags']:
+        shots_model_df.loc[i, 'header_tag'] = 1
+        
+    # Outcome
+    shots_model_df.loc[i, 'goal'] = 0
+    if {'id': 101} in shot['tags']:
+        shots_model_df.loc[i, 'goal'] = 1        
+    
+    i += 1
+
+
+#%% Train a neural network
+
+X = shots_model_df.drop(['player_name', 'team_name', 'competition_name','goal'], axis=1)
+y = shots_model_df['goal']
+
+# Build model
+neural_net = tf.keras.models.Sequential()
+neural_net.add(tf.keras.layers.Flatten())
+neural_net.add(tf.keras.layers.Dense(256, activation=tf.nn.relu))
+neural_net.add(tf.keras.layers.Dense(256, activation=tf.nn.relu))
+neural_net.add(tf.keras.layers.Dense(256, activation=tf.nn.relu))
+neural_net.add(tf.keras.layers.Dense(2, activation = tf.nn.softmax))
+
+# Compile model
+neural_net.compile(optimizer='adam',
+			  loss='sparse_categorical_crossentropy',
+			  metrics=['accuracy'])
+
+# Train model
+neural_net.fit(X, y, epochs=5)						
+
+# %% Make prediction
+
+#  xG
+shots_model_df['xG'] = neural_net.predict([X])[:,1]
+
+# %% Save xG data
+
+with bz2.BZ2File("../../data_directory/misc_data/neural_net_xg_data.pbz2", "wb") as f:
+    pickle.dump(shots_model_df, f)
+
+#%% Create an xG test-set, and predict on test set
+
+# Initialise dataframes for ground and header test sets
+X_test_grnd = pd.DataFrame(columns = ['xpos','cpos','distance','angle','header_tag'], dtype='float64')
+X_test_head = pd.DataFrame(columns = ['xpos','cpos','distance','angle','header_tag'], dtype='float64')
+i = 0
+
+# Create array of shots
+for x_pos in range(0,int(PITCH_LENGTH_X/2 + 1)):
+    for y_pos in range(0, int(PITCH_WIDTH_Y + 1)):
+        c_pos = y_pos - PITCH_WIDTH_Y/2
+        angle_denominator = (x_pos**2 + c_pos**2 - (GOAL_WIDTH_Y/2)**2)
+        if angle_denominator == 0:
+            angle = np.pi/2
+        else:
+            angle = np.arctan(2*(GOAL_WIDTH_Y/2)*x_pos/angle_denominator)
+        if angle < 0:
+            angle = np.pi + angle
+        distance = np.sqrt(x_pos**2 + c_pos**2)
+        X_test_grnd.loc[i,:] = [x_pos, c_pos, distance, angle, 0]
+        X_test_head.loc[i,:] = [x_pos, c_pos, distance, angle, 1]
+        i += 1
+
+prob_goal_grnd = neural_net.predict([X_test_grnd])[:,1].reshape(int(1+PITCH_LENGTH_X/2),int(1+PITCH_WIDTH_Y))
+prob_goal_head = neural_net.predict([X_test_head])[:,1].reshape(int(1+PITCH_LENGTH_X/2),int(1+PITCH_WIDTH_Y))
+
+# %% Plot xG model
+
+# Overwrite rcParams 
+mpl.rcParams['xtick.color'] = "white"
+mpl.rcParams['ytick.color'] = "white"
+mpl.rcParams['xtick.labelsize'] = 10
+mpl.rcParams['ytick.labelsize'] = 10
+
+# Plot pitches
+pitch = VerticalPitch(half=True,pitch_color='#313332', line_color='white', linewidth=1, stripe=False)
+fig, ax = pitch.grid(nrows=1, ncols=2, grid_height=0.75, space = 0.1, axis=False)
+fig.set_size_inches(10, 5.5)
+fig.set_facecolor('#313332')
+
+# Add xG maps and contours
+pos1 = ax['pitch'][0].imshow(prob_goal_grnd, extent = (80,0,60,120) ,aspect='equal',vmin=-0.04,vmax=0.4,cmap=plt.cm.inferno)
+pos2 = ax['pitch'][1].imshow(prob_goal_head, extent = (80,0,60,120) ,aspect='equal',vmin=-0.04,vmax=0.4,cmap=plt.cm.inferno)
+cs1 = ax['pitch'][0].contour(prob_goal_grnd, extent = (1,80,120,60), levels = [0.01,0.05,0.2,0.5], colors = ['darkgrey','darkgrey','darkgrey','k'], linestyles = 'dotted')
+cs2 = ax['pitch'][1].contour(prob_goal_head, extent = (1,80,120,60), levels = [0.01,0.05,0.2,0.5], colors = ['darkgrey','darkgrey','darkgrey','k'], linestyles = 'dotted')
+ax['pitch'][0].clabel(cs1)
+ax['pitch'][1].clabel(cs2)
+
+# Title
+fig.text(0.045,0.9,"Expected Goals - Neural Network", fontsize=16, color="white", fontweight="bold")
+fig.text(0.045,0.85,"Trained on all 40,000+ shots during the 2017/18 season across Europe's 'big five' Leagues", fontsize=14, color="white", fontweight="regular")
+fig.text(0.12,0.76,"Shot Type: Left or Right Foot", fontsize=12, color="white", fontweight="bold")
+fig.text(0.66,0.76,"Shot Type: Header", fontsize=12, color="white", fontweight="bold")
+
+# Colourbar
+cbar = fig.colorbar(pos2, ax=ax['pitch'][1], location="bottom",  fraction = 0.04, pad = 0.0335)
+cbar.ax.set_ylabel('xG', loc="bottom", color = "white", fontweight="bold", rotation=0, labelpad=20)
+
+# Footer text
+fig.text(0.255, 0.09, "Created by Jake Kolliari / @jakeyk11. Data provided by Wyscout.com",
+         fontstyle="italic", ha="center", fontsize=9, color="white")  
+
+# Format and show
+plt.tight_layout()
+plt.show()
