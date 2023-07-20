2bd8cc40178632b769a0f1f081c9da4a7d154593
jakeyk11 <jkolliari@hotmail.com>
2023-07-21 00:11:56 +0100

    Adjusted FBRef scraper to fix merging issue

--- projects/00_data_import_and_misc_work/import_data_fbref.py
+++ projects/00_data_import_and_misc_work/import_data_fbref.py
@@ -49,7 +49,7 @@ teaminfo_for_df = pd.DataFrame()
 teaminfo_against_df = pd.DataFrame()
 
 # Iterate over statistic type
-for statistic_group in list(fbref_dict.keys()):
+for idx, statistic_group in enumerate(list(fbref_dict.keys())):
     
     # Team stats for
     temp_team_stat_for_df = fbref_dict[statistic_group][0].copy()
@@ -57,10 +57,12 @@ for statistic_group in list(fbref_dict.keys()):
     for col_name in temp_team_stat_for_df.columns:
         col_name_1 = '' if 'Unnamed' in col_name[0] else col_name[0]
         col_name_2 = col_name[1] if 'Unnamed' in col_name[0] else ' ' + col_name[1]
-        new_col_names.append(col_name_1 + col_name_2)
+        new_col_names.append((col_name_1 + col_name_2).strip())
     temp_team_stat_for_df.columns = new_col_names
-    teaminfo_for_df = pd.concat([teaminfo_for_df, temp_team_stat_for_df], axis=1)
-    teaminfo_for_df = teaminfo_for_df.loc[:,~teaminfo_for_df.columns.duplicated()]
+    if idx != 0:
+        teaminfo_for_df = teaminfo_for_df.merge(temp_team_stat_for_df, left_on='Team ID', right_on='Team ID', suffixes=('', '_duplicate'), how = "outer") 
+    else:
+        teaminfo_for_df = temp_team_stat_for_df
     
     # Team stats against
     temp_team_stat_against_df = fbref_dict[statistic_group][1].copy()
@@ -68,10 +70,12 @@ for statistic_group in list(fbref_dict.keys()):
     for col_name in temp_team_stat_against_df.columns:
         col_name_1 = '' if 'Unnamed' in col_name[0] else col_name[0]
         col_name_2 = col_name[1] if 'Unnamed' in col_name[0] else ' ' + col_name[1]
-        new_col_names.append(col_name_1 + col_name_2)
+        new_col_names.append((col_name_1 + col_name_2).strip())
     temp_team_stat_against_df.columns = new_col_names
-    teaminfo_against_df = pd.concat([teaminfo_against_df, temp_team_stat_against_df], axis=1)
-    teaminfo_against_df = teaminfo_against_df.loc[:,~teaminfo_against_df.columns.duplicated()]
+    if idx != 0:
+        teaminfo_against_df = teaminfo_against_df.merge(temp_team_stat_against_df, left_on='Team ID', right_on='Team ID', suffixes=('', '_duplicate'), how = "outer") 
+    else:
+        teaminfo_against_df = temp_team_stat_against_df
 
     # Player stats
     temp_player_stat_df = fbref_dict[statistic_group][2].copy()
@@ -79,10 +83,24 @@ for statistic_group in list(fbref_dict.keys()):
     for col_name in temp_player_stat_df.columns:
         col_name_1 = '' if 'Unnamed' in col_name[0] else col_name[0]
         col_name_2 = col_name[1] if 'Unnamed' in col_name[0] else ' ' + col_name[1]
-        new_col_names.append(col_name_1 + col_name_2)
+        new_col_names.append((col_name_1 + col_name_2).strip())
     temp_player_stat_df.columns = new_col_names
-    playerinfo_df = pd.concat([playerinfo_df, temp_player_stat_df], axis=1)
-    playerinfo_df = playerinfo_df.loc[:,~playerinfo_df.columns.duplicated()]
+    if idx != 0:
+        playerinfo_df = playerinfo_df.merge(temp_player_stat_df, left_on=['Player ID', 'Squad'], right_on=['Player ID', 'Squad'], suffixes=('', '_duplicate'), how = "outer") 
+    else:
+        playerinfo_df = temp_player_stat_df
+
+# Remove duplicate columns
+teaminfo_for_df = teaminfo_for_df.loc[:,[False if '_duplicate' in x else True for x in teaminfo_for_df.columns]]
+teaminfo_against_df = teaminfo_against_df.loc[:,[False if '_duplicate' in x else True for x in teaminfo_against_df.columns]]
+playerinfo_df = playerinfo_df.loc[:,[False if '_duplicate' in x else True for x in playerinfo_df.columns]]
+       
+# Adjust data types
+for col_name in playerinfo_df.columns:
+    try:
+        playerinfo_df[col_name] = playerinfo_df[col_name].astype(float)
+    except:
+        pass
 
 # %% Save scraped data
 
